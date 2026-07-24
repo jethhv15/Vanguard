7c20f9b264066b8d1e1604ba613f0f747d67695d
jethhv15 <bapakeeee8@gmail.com>
2026-07-25 02:53:36 +0700

    feat(discovery): add system discovery manager

---
+++ core/discovery.sh
@@ -0,0 +1,32 @@
+#!/system/bin/sh
+#
+# Project Vanguard
+# Discovery Manager
+#
+
+#
+# Load Dependencies
+#
+
+CORE_DIR="$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)"
+
+. "$CORE_DIR/constants.sh"
+. "$CORE_DIR/detect.sh"
+. "$CORE_DIR/capability.sh"
+
+#
+# Public Functions
+#
+
+vg_discover() {
+
+    vg_detect_device
+    vg_detect_selinux
+    vg_detect_abi
+    vg_detect_root_manager
+
+    vg_detect_ab_slots
+    vg_detect_dynamic_partitions
+
+    return "$VG_SUCCESS"
+}
