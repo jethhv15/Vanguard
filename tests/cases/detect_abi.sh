2c298a98efa2912aef0d7b265eca183454d4b26f
jethhv15 <bapakeeee8@gmail.com>
2026-07-25 02:44:06 +0700

    feat(detect): add ABI detection

---
+++ tests/cases/detect_abi.sh
@@ -0,0 +1,21 @@
+#!/system/bin/sh
+#
+# Project Vanguard
+# ABI Detection Test
+#
+
+TEST_DIR="$(CDPATH= cd -- "$(dirname -- "$0")/.." && pwd)"
+CORE_DIR="$(CDPATH= cd -- "$TEST_DIR/../core" && pwd)"
+
+. "$TEST_DIR/testlib.sh"
+. "$CORE_DIR/constants.sh"
+. "$CORE_DIR/detect.sh"
+
+vg_assert_return_code \
+    "$VG_SUCCESS" \
+    "$(vg_detect_abi >/dev/null 2>&1; echo $?)" \
+    "vg_detect_abi returns success"
+
+vg_assert_true \
+    "[ -n \"$VG_ABI\" ]" \
+    "ABI should not be empty"
