24cf7ec5cb56819c46f6970e91b73ae999a26387
jethhv15 <bapakeeee8@gmail.com>
2026-07-25 03:49:01 +0700

    feat(loader): add initial module loader

--- core/engine.sh
+++ core/engine.sh
@@ -4,38 +4,21 @@
 # Engine
 #
 
-#
-# Load Dependencies
-#
-
 CORE_DIR="$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)"
 
 . "$CORE_DIR/constants.sh"
 . "$CORE_DIR/runtime.sh"
 . "$CORE_DIR/scanner.sh"
-
-#
-# Public Functions
-#
+. "$CORE_DIR/loader.sh"
 
 vg_engine_start() {
 
     vg_runtime_is_initialized || return "$VG_ERR_INTERNAL"
 
-    #
-    # Scan available modules
-    #
-
-    vg_scan_modules >/dev/null || return $?
-
-    #
-    # Module pipeline
-    #
-    # Parser
-    # Module Validator
-    # Loader
-    # Lifecycle
-    #
+    vg_scan_modules | while IFS= read -r module
+    do
+        vg_load_module "$module" || return $?
+    done
 
     return "$VG_SUCCESS"
 }
