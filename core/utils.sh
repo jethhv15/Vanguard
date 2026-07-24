e6833bd9264cf5eb6867f858a0de61a7d53e9edd
jethhv15 <bapakeeee8@gmail.com>
2026-07-25 01:50:51 +0700

    feat(utils): add common utility helpers

--- core/utils.sh
+++ core/utils.sh
@@ -1,1 +1,30 @@
+#!/system/bin/sh
+#
+# Project Vanguard
+# Utility Component
+#
 
+#
+# Load Dependencies
+#
+
+. "$(dirname "$0")/constants.sh"
+
+#
+# Public Functions
+#
+
+vg_command_exists() {
+    command -v "$1" >/dev/null 2>&1
+    return $?
+}
+
+vg_is_empty() {
+    [ -z "$1" ]
+    return $?
+}
+
+vg_is_not_empty() {
+    [ -n "$1" ]
+    return $?
+}
