489bd4349c9a6568a211f00721be7684a017665c
jethhv15 <bapakeeee8@gmail.com>
2026-07-25 01:35:03 +0700

    feat(validator): implement environment validation

--- core/validator.sh
+++ core/validator.sh
@@ -1,1 +1,31 @@
+#!/system/bin/sh
+#
+# Project Vanguard
+# Environment Validator
+#
 
+#
+# Load Dependencies
+#
+
+. "$(dirname "$0")/constants.sh"
+. "$(dirname "$0")/detect.sh"
+
+#
+# Public Functions
+#
+
+vg_validate_environment() {
+
+    vg_detect_device
+
+    if [ -z "$VG_DEVICE" ]; then
+        return "$VG_ERR_UNSUPPORTED"
+    fi
+
+    if [ -z "$VG_ANDROID" ]; then
+        return "$VG_ERR_UNSUPPORTED"
+    fi
+
+    return "$VG_SUCCESS"
+}
