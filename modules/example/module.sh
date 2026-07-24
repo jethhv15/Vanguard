9b0b86e0a0f3810094b877530f7eceeb7169b924
jethhv15 <bapakeeee8@gmail.com>
2026-07-25 03:12:24 +0700

    Create module.sh

---
+++ modules/example/module.sh
@@ -0,0 +1,20 @@
+#!/system/bin/sh
+#
+# Project Vanguard
+# Example Module
+#
+
+vg_module_init() {
+
+    return 0
+}
+
+vg_module_start() {
+
+    return 0
+}
+
+vg_module_stop() {
+
+    return 0
+}
