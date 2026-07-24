80f49d877ca9c3f789b4c85487fc28e2aba6b612
jethhv15 <bapakeeee8@gmail.com>
2026-07-25 01:31:34 +0700

    feat(core): implement shared constants

--- core/constants.sh
+++ core/constants.sh
@@ -1,1 +1,33 @@
+#!/system/bin/sh
+#
+# Project Vanguard
+# Shared Constants
+#
 
+#
+# Return Codes
+#
+
+VG_SUCCESS=0
+VG_ERR_GENERAL=1
+VG_ERR_CONFIG=2
+VG_ERR_UNSUPPORTED=3
+VG_ERR_DEPENDENCY=4
+VG_ERR_INTERNAL=5
+
+#
+# Log Levels
+#
+
+VG_LOG_INFO="INFO"
+VG_LOG_WARN="WARN"
+VG_LOG_ERROR="ERROR"
+VG_LOG_DEBUG="DEBUG"
+
+#
+# Project Information
+#
+
+VG_NAME="Project Vanguard"
+VG_VERSION="0.1.0"
+VG_AUTHOR="Jethh"
