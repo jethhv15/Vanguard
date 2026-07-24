1afd4a68d9770baa881c8f4e6aed4570266e020a
jethhv15 <bapakeeee8@gmail.com>
2026-07-25 02:15:06 +0700

    test(config): add basic configuration test cases

---
+++ tests/cases/config.sh
@@ -0,0 +1,25 @@
+#!/system/bin/sh
+#
+# Project Vanguard
+# Configuration Test Cases
+#
+
+TEST_DIR="$(CDPATH= cd -- "$(dirname -- "$0")/.." && pwd)"
+CORE_DIR="$(CDPATH= cd -- "$TEST_DIR/../core" && pwd)"
+CONFIG_DIR="$(CDPATH= cd -- "$TEST_DIR/../config" && pwd)"
+
+. "$TEST_DIR/testlib.sh"
+. "$CORE_DIR/constants.sh"
+. "$CORE_DIR/config.sh"
+
+VG_CONFIG_FILE="$CONFIG_DIR/default.conf"
+
+vg_assert_return_code \
+    "$VG_SUCCESS" \
+    "$(vg_config_load >/dev/null 2>&1; echo $?)" \
+    "vg_config_load loads valid configuration"
+
+vg_assert_equal \
+    "INFO" \
+    "$(vg_config_get VG_LOG_LEVEL)" \
+    "VG_LOG_LEVEL matches default configuration"
