71f833c9384727d7ad1beb2db39f87f4d3afbeaf
jethhv15 <bapakeeee8@gmail.com>
2026-07-25 01:34:14 +0700

    feat(detect): implement basic environment detection

--- core/detect.sh
+++ core/detect.sh
@@ -1,1 +1,38 @@
+#!/system/bin/sh
+#
+# Project Vanguard
+# Device Detection Component
+#
 
+#
+# Load Dependencies
+#
+
+. "$(dirname "$0")/constants.sh"
+
+#
+# Global Variables
+#
+
+VG_DEVICE=""
+VG_ANDROID=""
+VG_KERNEL=""
+VG_KERNELSU=""
+
+#
+# Public Functions
+#
+
+vg_detect_device() {
+    VG_DEVICE="$(getprop ro.product.device)"
+    VG_ANDROID="$(getprop ro.build.version.release)"
+    VG_KERNEL="$(uname -r)"
+
+    if [ -d "/data/adb/ksu" ] || [ -d "/data/adb/modules" ]; then
+        VG_KERNELSU="true"
+    else
+        VG_KERNELSU="false"
+    fi
+
+    return "$VG_SUCCESS"
+}
