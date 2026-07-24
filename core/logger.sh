9ae36aa4577796a6563efce60cb8a5028c993ac2
jethhv15 <bapakeeee8@gmail.com>
2026-07-25 01:56:47 +0700

    feat(logger): implement configurable log level filtering

--- core/logger.sh
+++ core/logger.sh
@@ -29,12 +29,50 @@ _vg_timestamp() {
     date '+%Y-%m-%d %H:%M:%S'
 }
 
+_vg_should_log() {
+    case "${VG_LOG_LEVEL:-INFO}:$1" in
+        DEBUG:DEBUG|DEBUG:INFO|DEBUG:WARN|DEBUG:ERROR) return 0 ;;
+        INFO:INFO|INFO:WARN|INFO:ERROR) return 0 ;;
+        WARN:WARN|WARN:ERROR) return 0 ;;
+        ERROR:ERROR) return 0 ;;
+        *) return 1 ;;
+    esac
+}
+
 #
 # Public Functions
 #
 
 vg_log() {
     level="$1"
+    shift
+
+    _vg_is_valid_level "$level" || return "$VG_ERR_GENERAL"
+
+    [ $# -gt 0 ] || return "$VG_ERR_GENERAL"
+
+    _vg_should_log "$level" || return "$VG_SUCCESS"
+
+    printf '%s [%s] %s\n' "$(_vg_timestamp)" "$level" "$*"
+
+    return "$VG_SUCCESS"
+}
+
+vg_info() {
+    vg_log "$VG_LOG_INFO" "$@"
+}
+
+vg_warn() {
+    vg_log "$VG_LOG_WARN" "$@"
+}
+
+vg_error() {
+    vg_log "$VG_LOG_ERROR" "$@"
+}
+
+vg_debug() {
+    vg_log "$VG_LOG_DEBUG" "$@"
+}    level="$1"
     shift
 
     _vg_is_valid_level "$level" || return "$VG_ERR_GENERAL"
