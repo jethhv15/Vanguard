d07f17c97178711112d05bef0b8e9243dc9aa168
jethhv15 <bapakeeee8@gmail.com>
2026-07-25 02:49:44 +0700

    feat(detect): separate root manager detection

--- core/detect.sh
+++ core/detect.sh
@@ -14,15 +14,23 @@
 # Global Variables
 #
 
+# Device
 VG_DEVICE=""
 VG_BRAND=""
 VG_MODEL=""
+
+# System
 VG_ANDROID=""
 VG_SDK=""
 VG_KERNEL=""
-VG_KERNELSU=""
 VG_SELINUX=""
+
+# Hardware
 VG_ABI=""
+
+# Environment
+VG_KERNELSU=""
+VG_ROOT_MANAGER=""
 
 #
 # Public Functions
@@ -35,12 +43,6 @@ vg_detect_device() {
     VG_ANDROID="$(getprop ro.build.version.release)"
     VG_SDK="$(getprop ro.build.version.sdk)"
     VG_KERNEL="$(uname -r)"
-
-    if [ -d "/data/adb/ksu" ] || [ -d "/data/adb/modules" ]; then
-        VG_KERNELSU="true"
-    else
-        VG_KERNELSU="false"
-    fi
 
     return "$VG_SUCCESS"
 }
@@ -64,6 +66,24 @@ vg_detect_abi() {
     fi
 
     VG_ABI="UNKNOWN"
+
+    return "$VG_ERR_UNSUPPORTED"
+}
+
+vg_detect_root_manager() {
+    VG_KERNELSU="false"
+    VG_ROOT_MANAGER="NONE"
+
+    if [ -d "/data/adb/ksu" ]; then
+        VG_KERNELSU="true"
+        VG_ROOT_MANAGER="KernelSU"
+        return "$VG_SUCCESS"
+    fi
+
+    if [ -d "/data/adb/modules" ]; then
+        VG_ROOT_MANAGER="Magisk"
+        return "$VG_SUCCESS"
+    fi
 
     return "$VG_ERR_UNSUPPORTED"
 }
