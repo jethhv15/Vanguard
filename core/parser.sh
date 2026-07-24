a437f5722b408f620ffac6059d9e58cc604cc858
jethhv15 <bapakeeee8@gmail.com>
2026-07-25 03:38:38 +0700

    feat(parser): add module manifest parser

---
+++ core/parser.sh
@@ -0,0 +1,77 @@
+#!/system/bin/sh
+#
+# Project Vanguard
+# Module Manifest Parser
+#
+
+#
+# Load Dependencies
+#
+
+CORE_DIR="$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)"
+
+. "$CORE_DIR/constants.sh"
+
+#
+# Runtime Variables
+#
+
+VG_MODULE_ID=""
+VG_MODULE_NAME=""
+VG_MODULE_VERSION=""
+VG_MODULE_VERSION_CODE=""
+VG_MODULE_AUTHOR=""
+VG_MODULE_DESCRIPTION=""
+VG_MODULE_API=""
+VG_MODULE_ENTRY=""
+
+#
+# Public Functions
+#
+
+vg_parse_manifest() {
+
+    manifest="$1"
+
+    [ -f "$manifest" ] || return "$VG_ERR_NOT_FOUND"
+
+    #
+    # Reset runtime context
+    #
+
+    VG_MODULE_ID=""
+    VG_MODULE_NAME=""
+    VG_MODULE_VERSION=""
+    VG_MODULE_VERSION_CODE=""
+    VG_MODULE_AUTHOR=""
+    VG_MODULE_DESCRIPTION=""
+    VG_MODULE_API=""
+    VG_MODULE_ENTRY=""
+
+    while IFS='=' read -r key value
+    do
+
+        case "$key" in
+
+            id) VG_MODULE_ID="$value" ;;
+
+            name) VG_MODULE_NAME="$value" ;;
+
+            version) VG_MODULE_VERSION="$value" ;;
+
+            versionCode) VG_MODULE_VERSION_CODE="$value" ;;
+
+            author) VG_MODULE_AUTHOR="$value" ;;
+
+            description) VG_MODULE_DESCRIPTION="$value" ;;
+
+            api) VG_MODULE_API="$value" ;;
+
+            entry) VG_MODULE_ENTRY="$value" ;;
+
+        esac
+
+    done < "$manifest"
+
+    return "$VG_SUCCESS"
+}
