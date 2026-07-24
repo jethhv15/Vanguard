e57d92342488622f277c2098f4ef3111ba44e431
jethhv15 <bapakeeee8@gmail.com>
2026-07-25 03:41:41 +0700

    feat(module-validator): add module manifest validator

---
+++ core/module_validator.sh
@@ -0,0 +1,37 @@
+#!/system/bin/sh
+#
+# Project Vanguard
+# Module Validator
+#
+
+#
+# Load Dependencies
+#
+
+CORE_DIR="$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)"
+
+. "$CORE_DIR/constants.sh"
+. "$CORE_DIR/parser.sh"
+
+#
+# Public Functions
+#
+
+vg_validate_module() {
+
+    [ -n "$VG_MODULE_ID" ] || return "$VG_ERR_INVALID"
+
+    [ -n "$VG_MODULE_NAME" ] || return "$VG_ERR_INVALID"
+
+    [ -n "$VG_MODULE_VERSION" ] || return "$VG_ERR_INVALID"
+
+    [ -n "$VG_MODULE_VERSION_CODE" ] || return "$VG_ERR_INVALID"
+
+    [ -n "$VG_MODULE_AUTHOR" ] || return "$VG_ERR_INVALID"
+
+    [ -n "$VG_MODULE_API" ] || return "$VG_ERR_INVALID"
+
+    [ -n "$VG_MODULE_ENTRY" ] || return "$VG_ERR_INVALID"
+
+    return "$VG_SUCCESS"
+}
