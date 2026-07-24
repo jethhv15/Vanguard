25042e5df1420a437398787faa893ddadc5addab
jethhv15 <bapakeeee8@gmail.com>
2026-07-25 01:33:25 +0700

    feat(config): implement configuration loader

--- core/config.sh
+++ core/config.sh
@@ -1,1 +1,42 @@
+#!/system/bin/sh
+#
+# Project Vanguard
+# Configuration Component
+#
 
+#
+# Load Dependencies
+#
+
+. "$(dirname "$0")/constants.sh"
+
+#
+# Constants
+#
+
+VG_CONFIG_FILE="$(dirname "$0")/../config/default.conf"
+
+#
+# Public Functions
+#
+
+vg_config_load() {
+    if [ ! -f "$VG_CONFIG_FILE" ]; then
+        return "$VG_ERR_CONFIG"
+    fi
+
+    # shellcheck source=/dev/null
+    . "$VG_CONFIG_FILE"
+
+    return "$VG_SUCCESS"
+}
+
+vg_config_get() {
+    eval "printf '%s\n' \"\${$1}\""
+}
+
+vg_config_set() {
+    eval "$1=\"$2\""
+
+    return "$VG_SUCCESS"
+}
