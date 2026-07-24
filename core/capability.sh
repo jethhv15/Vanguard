7278155af8e8b659a636e3e95f23af81fb35d9b8
jethhv15 <bapakeeee8@gmail.com>
2026-07-25 02:52:11 +0700

    feat(capability): add device capability detection

---
+++ core/capability.sh
@@ -0,0 +1,45 @@
+#!/system/bin/sh
+#
+# Project Vanguard
+# Capability Detection Component
+#
+
+#
+# Load Dependencies
+#
+
+. "$(dirname "$0")/constants.sh"
+
+#
+# Global Variables
+#
+
+VG_CAP_DYNAMIC_PARTITIONS="false"
+VG_CAP_AB="false"
+
+#
+# Public Functions
+#
+
+vg_detect_dynamic_partitions() {
+
+    if [ -d "/dev/block/by-name" ] && \
+       [ -e "/dev/block/by-name/super" ]; then
+        VG_CAP_DYNAMIC_PARTITIONS="true"
+        return "$VG_SUCCESS"
+    fi
+
+    return "$VG_ERR_UNSUPPORTED"
+}
+
+vg_detect_ab_slots() {
+
+    slot_suffix="$(getprop ro.boot.slot_suffix)"
+
+    if [ -n "$slot_suffix" ]; then
+        VG_CAP_AB="true"
+        return "$VG_SUCCESS"
+    fi
+
+    return "$VG_ERR_UNSUPPORTED"
+}
