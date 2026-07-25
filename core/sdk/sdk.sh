e84ad1f871106e1ad4c2f9c0a6a30b5530f9eff4
jethhv15 <bapakeeee8@gmail.com>
2026-07-25 13:03:22 +0700

    refactor(sdk): prepare extensible SDK loader

--- core/sdk/sdk.sh
+++ core/sdk/sdk.sh
@@ -6,6 +6,46 @@
 
 SDK_DIR="$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)"
 
+#
+# Framework APIs
+#
+
 . "$SDK_DIR/framework.sh"
+
+#
+# Module APIs
+#
+
 . "$SDK_DIR/module.sh"
-. "$SDK_DIR/logger.sh"
+
+#
+# Services
+#
+
+if [ -f "$SDK_DIR/logger.sh" ]; then
+    . "$SDK_DIR/logger.sh"
+fi
+
+if [ -f "$SDK_DIR/filesystem.sh" ]; then
+    . "$SDK_DIR/filesystem.sh"
+fi
+
+if [ -f "$SDK_DIR/property.sh" ]; then
+    . "$SDK_DIR/property.sh"
+fi
+
+if [ -f "$SDK_DIR/process.sh" ]; then
+    . "$SDK_DIR/process.sh"
+fi
+
+if [ -f "$SDK_DIR/config.sh" ]; then
+    . "$SDK_DIR/config.sh"
+fi
+
+if [ -f "$SDK_DIR/service.sh" ]; then
+    . "$SDK_DIR/service.sh"
+fi
+
+if [ -f "$SDK_DIR/permission.sh" ]; then
+    . "$SDK_DIR/permission.sh"
+fi
