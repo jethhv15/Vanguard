cb46a0e3b49892501c912e98f6e8ba5c19a4f6fb
jethhv15 <bapakeeee8@gmail.com>
2026-07-25 12:59:56 +0700

    feat(sdk): add framework API compatibility checks

--- core/sdk/framework.sh
+++ core/sdk/framework.sh
@@ -11,3 +11,12 @@ vg_framework_version() {
 vg_framework_api() {
     printf '%s\n' "$VG_API_VERSION"
 }
+
+vg_require_api() {
+
+    required_api="$1"
+
+    [ -n "$required_api" ] || return "$VG_ERR_INVALID"
+
+    [ "$VG_API_VERSION" -ge "$required_api" ]
+}
