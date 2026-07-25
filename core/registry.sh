4cf538c59cd1ffc587bba3eaa2f7f958e0f279fa
jethhv15 <bapakeeee8@gmail.com>
2026-07-25 12:51:24 +0700

    feat(registry): add path lookup API

--- core/registry.sh
+++ core/registry.sh
@@ -1,22 +1,19 @@
 #!/system/bin/sh
 #
 # Project Vanguard
-# Module Registry
+# Registry
 #
 
-CORE_DIR="$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)"
-
-. "$CORE_DIR/constants.sh"
-
-VG_LOADED_MODULE_COUNT=0
 VG_LOADED_MODULES=""
+VG_LOADED_MODULE_COUNT=0
+
+#
+# Public API
+#
 
 vg_registry_reset() {
-
-    VG_LOADED_MODULE_COUNT=0
     VG_LOADED_MODULES=""
-
-    return "$VG_SUCCESS"
+    VG_LOADED_MODULE_COUNT=0
 }
 
 vg_registry_add() {
@@ -24,17 +21,43 @@ vg_registry_add() {
     module_id="$1"
     module_path="$2"
 
-    [ -n "$module_id" ] || return "$VG_ERR_INVALID"
-    [ -n "$module_path" ] || return "$VG_ERR_INVALID"
+    [ -n "$module_id" ] || return 1
+    [ -n "$module_path" ] || return 1
+
+    entry="${module_id}|${module_path}"
 
     if [ -z "$VG_LOADED_MODULES" ]; then
-        VG_LOADED_MODULES="${module_id}|${module_path}"
+        VG_LOADED_MODULES="$entry"
     else
         VG_LOADED_MODULES="${VG_LOADED_MODULES}
-${module_id}|${module_path}"
+${entry}"
     fi
 
     VG_LOADED_MODULE_COUNT=$((VG_LOADED_MODULE_COUNT + 1))
+}
 
-    return "$VG_SUCCESS"
+vg_registry_get_path() {
+
+    module_id="$1"
+
+    [ -n "$module_id" ] || return 1
+
+    old_ifs="$IFS"
+    IFS='
+'
+
+    for entry in $VG_LOADED_MODULES
+    do
+        id="${entry%%|*}"
+        path="${entry#*|}"
+
+        if [ "$id" = "$module_id" ]; then
+            IFS="$old_ifs"
+            printf '%s\n' "$path"
+            return 0
+        fi
+    done
+
+    IFS="$old_ifs"
+    return 1
 }
