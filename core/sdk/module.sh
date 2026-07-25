6ba060579880c2a0943aa81c76bab77f19700055
jethhv15 <bapakeeee8@gmail.com>
2026-07-25 12:58:46 +0700

    feat(sdk): introduce modular SDK foundation

---
+++ core/sdk/module.sh
@@ -0,0 +1,25 @@
+#!/system/bin/sh
+#
+# Project Vanguard
+# SDK Module API
+#
+
+vg_module_id() {
+    printf '%s\n' "$VG_CURRENT_MODULE_ID"
+}
+
+vg_module_name() {
+    printf '%s\n' "$VG_CURRENT_MODULE_NAME"
+}
+
+vg_module_version() {
+    printf '%s\n' "$VG_CURRENT_MODULE_VERSION"
+}
+
+vg_module_author() {
+    printf '%s\n' "$VG_CURRENT_MODULE_AUTHOR"
+}
+
+vg_module_description() {
+    printf '%s\n' "$VG_CURRENT_MODULE_DESCRIPTION"
+}
