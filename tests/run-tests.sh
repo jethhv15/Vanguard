5d2ca329013f051b64e3ba9507ed2af28eecf686
jethhv15 <bapakeeee8@gmail.com>
2026-07-25 15:20:30 +0700

    refactor(tests): centralize project path initialization

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -5,6 +5,16 @@
 #
 
 TEST_DIR="$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)"
+PROJECT_DIR="$(CDPATH= cd -- "$TEST_DIR/.." && pwd)"
+CORE_DIR="$PROJECT_DIR/core"
+CONFIG_DIR="$PROJECT_DIR/config"
+MODULE_DIR="$PROJECT_DIR/modules"
+
+export TEST_DIR
+export PROJECT_DIR
+export CORE_DIR
+export CONFIG_DIR
+export MODULE_DIR
 
 . "$TEST_DIR/testlib.sh"
 
