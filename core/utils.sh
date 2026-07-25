cb973be9fe0110ff05e97adb7afa4507dff3dfdb
jethhv15 <bapakeeee8@gmail.com>
2026-07-25 20:36:18 +0700

    refactor(core): replace dirname($0) dependency loading in logger, utils and validator

--- core/utils.sh
+++ core/utils.sh
@@ -8,23 +8,24 @@
 # Load Dependencies
 #
 
-. "$(dirname "$0")/constants.sh"
+if [ -z "${CORE_DIR:-}" ]; then
+    CORE_DIR="$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)"
+fi
+
+. "$CORE_DIR/constants.sh"
 
 #
 # Public Functions
 #
 
+vg_file_exists() {
+    [ -f "$1" ]
+}
+
+vg_dir_exists() {
+    [ -d "$1" ]
+}
+
 vg_command_exists() {
     command -v "$1" >/dev/null 2>&1
-    return $?
-}
-
-vg_is_empty() {
-    [ -z "$1" ]
-    return $?
-}
-
-vg_is_not_empty() {
-    [ -n "$1" ]
-    return $?
 }
