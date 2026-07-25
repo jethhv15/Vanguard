6cbf15a94a32bdaf16414e619e477206886db291
jethhv15 <bapakeeee8@gmail.com>
2026-07-25 11:36:41 +0700

    refactor(engine): remove pipeline iteration to avoid subshell state loss

--- core/scanner.sh
+++ core/scanner.sh
@@ -1,37 +1,30 @@
 #!/system/bin/sh
 #
 # Project Vanguard
-# Module Scanner
-#
-
-#
-# Load Dependencies
+# Scanner
 #
 
 CORE_DIR="$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)"
 
 . "$CORE_DIR/constants.sh"
 
-#
-# Constants
-#
-
-VG_MODULES_DIR="${VG_MODULES_DIR:-modules}"
-
-#
-# Public Functions
-#
+VG_SCANNED_MODULES=""
 
 vg_scan_modules() {
 
-    [ -d "$VG_MODULES_DIR" ] || return "$VG_SUCCESS"
+    VG_SCANNED_MODULES=""
+
+    [ -d "$VG_MODULES_DIR" ] || return "$VG_ERR_NOT_FOUND"
 
     for module in "$VG_MODULES_DIR"/*; do
-
         [ -d "$module" ] || continue
 
-        printf '%s\n' "$module"
-
+        if [ -z "$VG_SCANNED_MODULES" ]; then
+            VG_SCANNED_MODULES="$module"
+        else
+            VG_SCANNED_MODULES="${VG_SCANNED_MODULES}
+$module"
+        fi
     done
 
     return "$VG_SUCCESS"
