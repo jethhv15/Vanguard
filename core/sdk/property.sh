6fe19c778ff15b0b84edd8fcd6b5e9e4f6e79ef5
jethhv15 <bapakeeee8@gmail.com>
2026-07-25 13:07:55 +0700

    feat(sdk): add property access API

--- core/sdk/property.sh
+++ core/sdk/property.sh
@@ -9,13 +9,29 @@ vg_property_get() {
     key="$1"
 
     case "$key" in
-        id)          printf '%s\n' "$VG_CURRENT_MODULE_ID" ;;
-        name)        printf '%s\n' "$VG_CURRENT_MODULE_NAME" ;;
-        version)     printf '%s\n' "$VG_CURRENT_MODULE_VERSION" ;;
-        author)      printf '%s\n' "$VG_CURRENT_MODULE_AUTHOR" ;;
-        description) printf '%s\n' "$VG_CURRENT_MODULE_DESCRIPTION" ;;
-        api)         printf '%s\n' "$VG_CURRENT_MODULE_API" ;;
-        path)        printf '%s\n' "$VG_CURRENT_MODULE_PATH" ;;
-        *) return 1 ;;
+        id)
+            printf '%s\n' "$VG_CURRENT_MODULE_ID"
+            ;;
+        name)
+            printf '%s\n' "$VG_CURRENT_MODULE_NAME"
+            ;;
+        version)
+            printf '%s\n' "$VG_CURRENT_MODULE_VERSION"
+            ;;
+        author)
+            printf '%s\n' "$VG_CURRENT_MODULE_AUTHOR"
+            ;;
+        description)
+            printf '%s\n' "$VG_CURRENT_MODULE_DESCRIPTION"
+            ;;
+        api)
+            printf '%s\n' "$VG_CURRENT_MODULE_API"
+            ;;
+        path)
+            printf '%s\n' "$VG_CURRENT_MODULE_PATH"
+            ;;
+        *)
+            return 1
+            ;;
     esac
 }
