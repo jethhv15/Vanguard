28140f2fc05d88a19f9031dc20c99cda8ecf24de
jethhv15 <bapakeeee8@gmail.com>
2026-07-25 11:57:56 +0700

    refactor(registry): store module id and path

--- core/registry.sh
+++ core/registry.sh
@@ -4,24 +4,12 @@
 # Module Registry
 #
 
-#
-# Load Dependencies
-#
-
 CORE_DIR="$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)"
 
 . "$CORE_DIR/constants.sh"
 
-#
-# Global Variables
-#
-
 VG_LOADED_MODULE_COUNT=0
 VG_LOADED_MODULES=""
-
-#
-# Public Functions
-#
 
 vg_registry_reset() {
 
@@ -34,14 +22,16 @@ vg_registry_reset() {
 vg_registry_add() {
 
     module_id="$1"
+    module_path="$2"
 
     [ -n "$module_id" ] || return "$VG_ERR_INVALID"
+    [ -n "$module_path" ] || return "$VG_ERR_INVALID"
 
     if [ -z "$VG_LOADED_MODULES" ]; then
-        VG_LOADED_MODULES="$module_id"
+        VG_LOADED_MODULES="${module_id}|${module_path}"
     else
         VG_LOADED_MODULES="${VG_LOADED_MODULES}
-$module_id"
+${module_id}|${module_path}"
     fi
 
     VG_LOADED_MODULE_COUNT=$((VG_LOADED_MODULE_COUNT + 1))
