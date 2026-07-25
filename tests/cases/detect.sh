f6adf20f0097ca294fa4592ee3e36538c4eb30fd
jethhv15 <bapakeeee8@gmail.com>
2026-07-25 22:10:42 +0700

    test(detect): align tests with detection API

--- tests/cases/detect.sh
+++ tests/cases/detect.sh
@@ -8,12 +8,13 @@
 . "$CORE_DIR/constants.sh"
 . "$CORE_DIR/detect.sh"
 
-vg_detect_environment >/dev/null 2>&1
+vg_detect_system >/dev/null 2>&1
+vg_detect_abi >/dev/null 2>&1
 
 vg_assert_true \
-    "[ -n \"$VG_ANDROID_VERSION\" ]" \
+    "[ -n \"$VG_ANDROID\" ]" \
     "Android version detected"
 
 vg_assert_true \
-    "[ -n \"$VG_DEVICE_ARCH\" ]" \
+    "[ -n \"$VG_ABI\" ]" \
     "Device architecture detected"
