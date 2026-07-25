46de5f10a275bf14b1fb7f9726fe2f354b390d48
jethhv15 <bapakeeee8@gmail.com>
2026-07-25 17:57:05 +0700

    refactor(test): use exported project paths in test cases

--- tests/cases/sdk_hook.sh
+++ tests/cases/sdk_hook.sh
@@ -1,8 +1,5 @@
 #!/system/bin/sh
 
-TEST_DIR="$(CDPATH= cd -- "$(dirname -- "$0")/.." && pwd)"
-CORE_DIR="$(CDPATH= cd -- "$TEST_DIR/../core" && pwd)"
-
 . "$CORE_DIR/sdk/sdk.sh"
 
 vg_register_hook() {
