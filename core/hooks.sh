83509f136b61d94b1d976a832303bd357786d405
jethhv15 <bapakeeee8@gmail.com>
2026-07-25 13:20:22 +0700

    feat(core): implement hook registry

--- core/hooks.sh
+++ core/hooks.sh
@@ -4,7 +4,19 @@
 # Hook Framework
 #
 
+VG_HOOKS=""
+
 vg_register_hook() {
+
+    hook="$1"
+    callback="$2"
+
+    [ -n "$hook" ] || return 1
+    [ -n "$callback" ] || return 1
+
+    VG_HOOKS="${VG_HOOKS}${hook}|${callback}
+"
+
     return 0
 }
 
