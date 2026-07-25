bb93eb23ef3d13bc12a855aa674457b8121c08fb
jethhv15 <bapakeeee8@gmail.com>
2026-07-25 13:31:53 +0700

    feat(core): introduce event framework contract

---
+++ core/event.sh
@@ -0,0 +1,17 @@
+#!/system/bin/sh
+#
+# Project Vanguard
+# Event Framework
+#
+
+vg_subscribe_event() {
+    return "$VG_SUCCESS"
+}
+
+vg_unsubscribe_event() {
+    return "$VG_SUCCESS"
+}
+
+vg_emit_event() {
+    return "$VG_SUCCESS"
+}
