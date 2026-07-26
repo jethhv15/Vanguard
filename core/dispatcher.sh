452711e0f5769f3843fb8414740b43ed0dee9e5d
jethhv15 <bapakeeeee8@gmail.com>
2026-07-26 14:42:44 +0700

    Core: migrate dispatcher to multi module state

--- core/dispatcher.sh
+++ core/dispatcher.sh
@@ -30,57 +30,73 @@ vg_dispatch_module() {
     [ -n "$action" ] || return "$VG_ERR_INVALID"
 
 
+
     module_path="$(vg_registry_get_path "$module_id")" \
         || return $?
 
 
+
     manifest="$module_path/module.prop"
+
 
 
     vg_parse_manifest "$manifest" \
         || return $?
 
 
+
     vg_context_set "$module_path" \
         || return $?
 
 
+
     callback="vg_${VG_MODULE_ID}_${action}"
+
+
+
+    #
+    # State validation
+    #
+
+    current_state="$(vg_state_get_module "$module_id" 2>/dev/null)"
+
 
 
     case "$action" in
 
         init)
 
-            vg_state_set "$VG_MODULE_STATE_LOADED" \
-                || {
-                    vg_context_clear
-                    return $?
-                }
+            if [ -n "$current_state" ]; then
+                vg_context_clear
+                return "$VG_ERR_INVALID"
+            fi
 
             ;;
+
 
 
         start)
 
-            [ "$VG_CURRENT_MODULE_STATE" = "$VG_MODULE_STATE_LOADED" ] \
+            [ "$current_state" = "loaded" ] \
                 || {
                     vg_context_clear
                     return "$VG_ERR_INVALID"
                 }
 
             ;;
+
 
 
         stop)
 
-            [ "$VG_CURRENT_MODULE_STATE" = "$VG_MODULE_STATE_STARTED" ] \
+            [ "$current_state" = "started" ] \
                 || {
                     vg_context_clear
                     return "$VG_ERR_INVALID"
                 }
 
             ;;
+
 
 
         *)
@@ -93,37 +109,70 @@ vg_dispatch_module() {
     esac
 
 
+
+    #
+    # Execute callback
+    #
+
     vg_invoke_callback "$callback"
+
     result=$?
+
 
 
     if [ "$result" -ne "$VG_SUCCESS" ]; then
 
         vg_context_clear
+
         return "$result"
 
     fi
 
 
+
+    #
+    # Update module state
+    #
+
     case "$action" in
 
         init)
-            vg_state_set "$VG_MODULE_STATE_LOADED"
+
+            vg_state_set \
+                "$module_id" \
+                "loaded"
+
             ;;
+
 
         start)
-            vg_state_set "$VG_MODULE_STATE_STARTED"
+
+            vg_state_set \
+                "$module_id" \
+                "started"
+
             ;;
 
+
         stop)
-            vg_state_set "$VG_MODULE_STATE_STOPPED"
+
+            vg_state_set \
+                "$module_id" \
+                "stopped"
+
             ;;
 
     esac
 
 
+
+    result=$?
+
+
+
     vg_context_clear
 
 
-    return "$VG_SUCCESS"
+
+    return "$result"
 }
